a95ba68b513300bafb7f920b050bf7038365ab73
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-11 21:58:21 +0545

    created route for pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import  Home  from './Pages/Home';
-import Services from './Pages/Services';
-import Work from './Pages/Work';
-import Resume from './Pages/Resume';
-import Contact from './Pages/Contact';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
 function App() {
   return (
-    <Routes>
-      <Route path='/home' element={<Home />} >
-        <Route path='service' element={<Services />} />
-        <Route path='work' element={<Work />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='resume' element={<Resume />} />
-      </Route>
-    </Routes>
-  );
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer/>
+    </>
+  )
 }
 
 export default App
