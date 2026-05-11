b949580a47c7b60a2d281b3cae6817acdd341392
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-11 22:02:49 +0545

    add some elements

---
+++ client/src/Components/Herosection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Herosection = () => {
+    return (
+        <>
+            <div className='max-h-full max-w-full border-2 border-red-500'>
+                <div>Box1</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+                <div>Box2</div>
+            </div>
+        </>
+    )
+}
+
+export default Herosection
