d7b44d6ca99480e692c23493a16c28b3a893d27b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-11 22:01:21 +0545

    fixed router error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,29 @@
+import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import Work from './Pages/Work';
+import Resume from './Pages/Resume';
+import Contact from './Pages/Contact';
+import PageWrapper from './animate/PageWrapper'
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='home' element={<Home/>}/>
+      <Route path='service' element={<Services/>}/>
+      <Route path='work' element={<Work/>}/>
+      <Route path='resume' element={<Resume/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <PageWrapper><RouterProvider router = {router}/></PageWrapper>
+  </React.StrictMode>
 )
