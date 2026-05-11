d5a0be61c3e0b67e39621766d3f8f82c029beb55
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-11 22:00:05 +0545

    created herosection

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../Components/Navbar"
+import Herosection from "../Components/Herosection"
 
 const Home = () => {
     return (
         <>
-            <Navbar />
-            <Outlet />
+        <Herosection/>
         </>
     )
 }
