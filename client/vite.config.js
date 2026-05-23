73c6529b2ad194370564d881b081ffab4079cb46
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-23 23:13:18 +0545

    chore: update vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react-dom') || id.includes('node_modules/react/')) {
+            return 'vendor'
+          }
+          if (id.includes('node_modules/react-router')) {
+            return 'router'
+          }
+          if (id.includes('node_modules/framer-motion')) {
+            return 'motion'
+          }
+        },
+      },
+    },
+  },
 })
