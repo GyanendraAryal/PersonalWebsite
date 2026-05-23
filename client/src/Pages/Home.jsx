419df50bdbf499cc9a44cd9e5889492702fa54da
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-23 23:13:18 +0545

    feat: update home page content and layout

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,13 +1,7 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Components/Navbar"
-import HeroSlider from "../Components/HeroSlider"
+import HeroSlider from '../Components/HeroSlider'
 
 const Home = () => {
-    return (
-        <>
-        <HeroSlider/>
-        </>
-    )
+  return <HeroSlider />
 }
 
 export default Home
