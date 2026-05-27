c2b0207c14c2d02a99ff330cad44e62c70976da5
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-05-27 21:09:57 +0545

    Fix Django static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,7 +79,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
